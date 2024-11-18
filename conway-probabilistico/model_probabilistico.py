6d61c0f1d0327258a46b9b029fd5f1d8542b3bb0
Anderson Aquiles <95103268+andersonaquiles35@users.noreply.github.com>
2024-11-18 17:59:11 -0300

    Revert "Alteracao aquiles"

---
+++ conway-probabilistico/model_probabilistico.py
@@ -0,0 +1,107 @@
+import numpy as np
+from mesa import Model
+from mesa.datacollection import DataCollector
+from mesa.space import PropertyLayer
+from scipy.signal import convolve2d
+
+# Usei uma distribuição de probabilidade onde seu acumulado se aproxima cada vez mais devagar de 1
+from scipy.stats import expon
+
+
+class GameOfLifeModel(
+    Model
+):  # Aqui eu adicionei o revive_probabilities e o survive_probabilities
+    def __init__(
+        self,
+        width=10,
+        height=10,
+        revive_probabilities=None,
+        survive_probabilities=None,
+        alive_fraction=0.2,
+        lamb=1000,
+        age_death=True,
+    ):
+        super().__init__()
+        # Adicionei o parametro lambida da distibuição de probabilidade
+        # Determina se a morte por idade está habilitado
+        self.age_death = age_death
+        # Initialize the property layer for cell states
+        self.cell_layer = PropertyLayer("cells", width, height, False, dtype=bool)
+        # Defino a idade de cada celula
+        self.age_layer = PropertyLayer("ages", width, height, 0, dtype=int)
+        self.cell_layer.data = np.random.choice(
+            [True, False], size=(width, height), p=[alive_fraction, 1 - alive_fraction]
+        )
+
+        # Caso a probabilidade não seja dada, o padrão que o código vai seguir é o determinístico do jogo de Conway
+        self.revive_probabilities = (
+            revive_probabilities if revive_probabilities is not None else {3: 1.0}
+        )
+        self.survive_probabilities = (
+            survive_probabilities
+            if survive_probabilities is not None
+            else {2: 1.0, 3: 1.0}
+        )
+
+        # Parametro lambida
+        self.lamb = lamb
+
+        # Metrics and datacollector
+        self.cells = width * height
+        self.alive_count = 0
+        self.alive_fraction = 0
+        self.datacollector = DataCollector(
+            model_reporters={
+                "Cells alive": "alive_count",
+                "Fraction alive": "alive_fraction",
+            }
+        )
+        self.datacollector.collect(self)
+
+    def step(self):
+        # Define a kernel for counting neighbors
+        kernel = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+
+        # Count neighbors
+        neighbor_count = convolve2d(
+            self.cell_layer.data, kernel, mode="same", boundary="wrap"
+        )
+
+        # Apply custom probabilistic rules for each cell
+        new_state = np.zeros_like(self.cell_layer.data, dtype=bool)
+        for x in range(self.cell_layer.data.shape[0]):
+            for y in range(self.cell_layer.data.shape[1]):
+                alive = self.cell_layer.data[x, y]
+                neighbors = neighbor_count[x, y]
+                idade = self.age_layer.data[x, y]
+                if alive:
+                    # Apply survival probability if the cell is alive
+                    survival_prob = self.survive_probabilities.get(neighbors, 0)
+                    # probablidade de morrre ( Quanto maior o lambida, menor vai ser com o tempo)
+                    morte_prob = 0
+                    if self.age_death:
+                        morte_prob = expon.cdf(idade, scale=self.lamb)
+                    # Retorna se consegiu ou não sobreviver
+                    viva = np.random.rand() < survival_prob
+                    # Retorna se morreu ou não
+                    morta = np.random.rand() < morte_prob
+                    if viva and not morta:
+                        new_state[x, y] = True
+                    else:
+                        new_state[x, y] = False
+                    # Altera a idade
+                    if not new_state[x, y]:
+                        self.age_layer.data[x, y] = 0
+                    else:
+                        self.age_layer.data[x, y] += 1
+                else:
+                    # Apply revival probability if the cell is dead
+                    revival_prob = self.revive_probabilities.get(neighbors, 0)
+                    new_state[x, y] = np.random.rand() < revival_prob
+
+        self.cell_layer.data = new_state
+
+        # Update metrics
+        self.alive_count = np.sum(self.cell_layer.data)
+        self.alive_fraction = self.alive_count / self.cells
+        self.datacollector.collect(self)
---
+++ different_visualizations/app.py
@@ -0,0 +1,49 @@
+from mesa.visualization import SolaraViz, make_plot_measure, make_space_matplotlib
+from model import GameOfLifeModel
+
+propertylayer_portrayal = {
+    "cell_layer": {
+        "color": "Black",
+        "alpha": 1,
+        "colorbar": False,
+    },
+}
+
+model_params = {
+    "width": {
+        "type": "SliderInt",
+        "value": 30,
+        "label": "Width",
+        "min": 5,
+        "max": 60,
+        "step": 1,
+    },
+    "height": {
+        "type": "SliderInt",
+        "value": 30,
+        "label": "Height",
+        "min": 5,
+        "max": 60,
+        "step": 1,
+    },
+    "alive_fraction": {
+        "type": "SliderFloat",
+        "value": 0.2,
+        "label": "Cells alive",
+        "min": 0,
+        "max": 1,
+        "step": 0.01,
+    },
+}
+
+gol = GameOfLifeModel()
+
+layer_viz = make_space_matplotlib(propertylayer_portrayal=propertylayer_portrayal)
+TotalAlivePlot = make_plot_measure("Cells alive")
+
+page = SolaraViz(
+    gol,
+    components=[layer_viz, TotalAlivePlot],
+    model_params=model_params,
+    name="Game of Life Model",
+)
